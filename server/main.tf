e907507b6b8aeb4ee1ab4a075ea44e56f40d9108
chavo1 <chavdar.rakov@gmail.com>
2019-11-28 11:49:43 +0200

    name

--- server/main.tf
+++ server/main.tf
@@ -3,6 +3,10 @@ resource "aws_instance" "web" {
   ami           = "ami-04169656fea786776"
   instance_type = "t2.micro"
   key_name      = "chavo"
+
+  tags = {
+    Name = "chavo-server0${count.index + 1}"
+  }
 }
 resource "aws_ec2_transit_gateway" "example" {
   description = "example"
