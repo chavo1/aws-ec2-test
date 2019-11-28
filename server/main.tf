b2e98bc59ca710a775a8e59de22ff04cd06e0359
chavo1 <chavdar.rakov@gmail.com>
2019-11-28 10:50:44 +0200

    count

--- server/main.tf
+++ server/main.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "web" {
+  count         = var.server_count
   ami           = "ami-04169656fea786776"
   instance_type = "t2.micro"
   key_name      = "chavo"
--- server/output.tf
+++ server/output.tf
@@ -1,8 +1,8 @@
 output "public_ip" {
-  value = aws_instance.web.public_ip
+  value = aws_instance.web.*.public_ip
 }
 output "public_dns" {
-  value = aws_instance.web.public_dns
+  value = aws_instance.web.*.public_dns
 }
 output "transit_gateway_arn" {
   value = aws_ec2_transit_gateway.example.arn
