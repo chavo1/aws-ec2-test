cbe138fd6852ee3a300aad13bdafcfd814ae0204
chavdar <chavdar.rakov@gmail.com>
2020-04-08 03:25:51 +0300

    Update azure.tf

--- azure.tf
+++ azure.tf
@@ -1,5 +1,6 @@
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
+  features {}
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
